88cbaca0f138306d3717dbad652a0ad7bac10bbc
katsitovlis <tovliskatsi@gmail.com>
2022-08-13 13:05:25 +0400

    do some

--- src/Engine2D/Scene.java
+++ src/Engine2D/Scene.java
@@ -28,7 +28,7 @@ public class Scene extends JPanel {
     private int bordersize = 0;
     private ShapesObject border = new ShapesObject("Border", 1);
     private boolean Vborder = false;
-
+    //do some
     public Scene(int w, int h){
         super();
         WIDTH = w;
