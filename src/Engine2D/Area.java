87a79940266f4bd6f29a9ece019b464357741b3e
TovlisKatsi <tovliskatsi@gmail.com>
2022-08-14 18:08:16 +0400

    Add class Area Alter colored paint in class Circle

--- src/Engine2D/Area.java
+++ src/Engine2D/Area.java
@@ -5,40 +5,23 @@ import UnityMath.Vector2;
 import java.awt.*;
 import java.util.ArrayList;
 
-/**Class 2D Circle*/
-public class Circle extends AbstractShape{
-    /**Circle constructor for oval
-     * ini height, width, position, color
-     * compute and add vertices for circle*/
-    public Circle(int height, int width, Vector2 pos, Color c){
-        super(c);//ini AbstractShape constructor
-        this.height = height;//ini height
-        this.width = width;//ini width
-        this.position = new Vector2(pos);//ini position
-        this.center = new Vector2(0,0);//ini center 0,0
-        for(double angle = 0.0; angle <= Math.PI * 2; angle += 1.0/Math.min(this.width, this.height)) {
-            double x = this.width * Math.sin(angle);
-            double y = this.height * Math.cos(angle);
-            this.vertices.add(new Vector2((float) x,(float) y));//add point in vertices
-        }
-    }
-    /**Circle constructor for circle
-     * ini height, width, position, color
-     * compute and add vertices for circle
-     * radius = width = height*/
-    public Circle(int radius, Vector2 pos, Color c){
-        super(c);//ini AbstractShape constructor
-        this.position = new Vector2(pos);//ini position
-        this.center = new Vector2(0,0);//ini center 0,0
+public class Area extends AbstractShape{
+    public double start;
+    public double end;
+    public Area(int radius, double start, double end, Vector2 pos, Color c){
+        super(c);
         this.height = radius;//ini height
         this.width = radius;//ini width
-        for(double angle = 0.0; angle <= Math.PI * 2; angle += 1.0/ this.width) {
+        this.start = start;
+        this.end = end;
+        this.position = new Vector2(pos);//ini position
+        this.center = new Vector2(0,0);//ini center 0,0
+        for(double angle = start; angle <= end; angle += 1.0/Math.min(this.width, this.height)) {
             double x = this.width * Math.sin(angle);
             double y = this.height * Math.cos(angle);
             this.vertices.add(new Vector2((float) x,(float) y));//add point in vertices
         }
     }
-    /**Method for paint on scene*/
     @Override
     public void paint(Graphics g, ShapeObject o) {
         ArrayList<Vector2> dots = getVertices(this.vertices);//get vertices for paint in screen dimension
@@ -57,18 +40,24 @@ public class Circle extends AbstractShape{
         for(int i = 0; i < dots.size()-1; ++i){
             Brezenheim(dots.get(i), dots.get(i+1), g);//paint line form i to i+1 point
         }
-        Brezenheim(dots.get(dots.size()-1), dots.get(0), g);//paint line from last to first point
+        Brezenheim(dots.get(dots.size()-1), zero, g);
+        Brezenheim(dots.get(0), zero, g);//paint line from last to first point
         if(this.CENTER) {//if center flag true paint center point
             g.setColor(Color.RED);//set color for center point RED
             g.fillRect((int) zero.x, (int) zero.y, 2, 2);//paint center point
         }
     }
-    /**Method for resize object*/
+
     @Override
     public void resize() {
         this.vertices.clear();//clear old vertices
         this.center = new Vector2(this.position);//ini new center TODO
-        for(double angle = 0.0; angle <= Math.PI * 2; angle += 1.0/Math.min(this.width, this.height)) {
+        if(end < start){
+            var tmp = start;
+            start = end;
+            end = tmp;
+        }
+        for(double angle = start; angle <= end; angle += 1.0/Math.min(this.width, this.height)) {
             double x = this.width * Math.sin(angle);
             double y = this.height * Math.cos(angle);
             this.vertices.add(new Vector2((float) x,(float) y));//add new vertices
