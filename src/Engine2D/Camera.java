f9f7440f766b7045249bb773411ec8a1769396d6
TovlisKatsi <tovliskatsi@gmail.com>
2022-08-14 10:39:17 +0400

    Add coments and docs in class Camera

--- src/Engine2D/Camera.java
+++ src/Engine2D/Camera.java
@@ -4,17 +4,14 @@ import UnityMath.Vector2;
 
 /**Class for 2D camera*/
 public class Camera {
-    public Vector2 position;
-
+    public Vector2 position;//camera position
+    /**Camera constructor
+     * ini camera position.*/
     Camera(){
         this.position = new Vector2(0,0);
     }
-
-    /**Fake camera: move world cords instead camera projection*/
+    /**Fake camera: move world coords instead camera projection*/
     public Vector2 Projection(Vector2 v){
-        int divX = (int)this.position.x;
-        int divY = (int)this.position.y;
-
-        return new Vector2(v.x  - divX,v.y - divY);
+        return new Vector2(v.x  - this.position.x,v.y - this.position.y);//move point in opposite direction from camera position
     }
 }
