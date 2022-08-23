41908ff11c095dada9beaded88ad0167ec869f19
TovlisKatsi <tovliskatsi@gmail.com>
2022-08-23 16:29:47 +0400

    Segregate mwthod resize and paint in two different interfaces

---
+++ src/Engine2D/Painting.java
@@ -0,0 +1,9 @@
+package Engine2D;
+
+import java.awt.*;
+
+/**Interface for print Shapes on scene*/
+public interface Painting {
+    /**pint object on scene*/
+    void paint(Graphics g, ShapeObject o);
+}
